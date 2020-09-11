776dd74d8fc7e660f6f526c3c55817758ae74f19
Michael Naquin <mnaquin1@yahoo.com>
2020-09-11 19:38:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ array[i] = changeValue
 }
 return array
 }
+
+function keepGoing(array, changeValue, skipValue){
+  for (let i = 0; i < array.length; i++) {
+  if (array[i] === skipValue) {
+continue 
+} else {
+  array[i] = changeValue
+}
+return array
+  }
+  
